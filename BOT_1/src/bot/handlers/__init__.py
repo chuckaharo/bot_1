d297ff8ddac503d25b2eaec4f37ba25761ca5dc6
chuckaharo <151360475+chuckaharo@users.noreply.github.com>
2025-03-02 15:22:10 -0600

    Delete BOT_1 directory

--- BOT_1/src/bot/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .commands import register_commands
-from .admin import register_admin_handlers
-from .payments import register_payment_handlers
-
-def register_handlers(application):
-    """Регистрация всех обработчиков команд и callback-ов"""
-    register_commands(application)
-    register_admin_handlers(application)
-    register_payment_handlers(application)
